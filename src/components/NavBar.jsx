f18eeeb1ea9a74379ce0f160fde1e4166406a9e3
lidwan <contact@loayidwan.com>
2025-02-11 21:42:17 +0300

    Added styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg bg-black navbar-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="https://loayidwan.com/">Loay Idwan</a>
+                <a className="navbar-brand" id="title" href="https://loayidwan.com/">Loay Idwan</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
