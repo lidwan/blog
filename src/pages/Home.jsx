5226e9600037fc8e80a1faf92721df9435764832
lidwan <contact@loayidwan.com>
2025-02-11 22:49:50 +0300

    Created two main types of pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar.jsx";
+import Footer from "../components/Footer.jsx";
+
+
+function Home() {
+    return (
+        <>
+            <NavBar />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
