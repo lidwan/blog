01d461925980ef0c84074da3bcb056e95ab37f26
lidwan <contact@loayidwan.com>
2025-02-12 15:54:13 +0300

    Added recent posts in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import NavBar from "../components/NavBar.jsx";
 import Footer from "../components/Footer.jsx";
 import PostCard from "../components/PostCard.jsx";
+import posts from "../data/posts.js";
 
 export default function Home() {
     return (
         <>
             <NavBar />
             <div className="mainHomeContainer">
-                <PostCard/>
+                <div className="welcomeTitle align-self-center  text-center">
+                    <h1 className="mb-3">Welcome to My Blog</h1>
+                    <h5>Sharing thoughts on tech, coding, and open source.</h5>
+                </div>
+                <h2 className="rec">Recent Posts</h2>
+                {posts.map((post) => (
+                    <PostCard post={post} key={post.id} />
+                ))}
             </div>
             <Footer/>
         </>
