f6babb4744c58aaa61265e3bf12312e7582b0ff2
lidwan <contact@loayidwan.com>
2025-02-12 18:59:23 +0300

    Fix GitHub Pages deployment (hopefully2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
 import NotFound from "./pages/NotFound";
