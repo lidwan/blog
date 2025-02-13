0a1bffefd661fc47f13b67e9d85b8e386413a6ac
lidwan <contact@loayidwan.com>
2025-02-13 19:57:06 +0300

    Fixed wrong link in navbar - 2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,7 +20,7 @@ function NavBar() {
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page"
-                               href="/">Blog</a>
+                               href="https://blog.loayidwan.com/">Blog</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="https://loayidwan.com/pages/projects.html">Projects</a>
