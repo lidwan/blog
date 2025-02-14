3cf7cf96f401a304bdfb9986ea6961ab984073a8
lidwan <contact@loayidwan.com>
2025-02-14 18:55:30 +0300

    Removed "icons by" in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <p className="footer">&copy; {currentYear} Loay&#39;s Blog, Icons by &nbsp;<a href="https://icons8.com">icons8</a></p>
+        <p className="footer">&copy; {currentYear} Loay&#39;s Blog</p>
     )
 }
 
