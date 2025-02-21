7987c83068b03bbf035bb31d4b54f83eb09d8c53
lidwan <contact@loayidwan.com>
2025-02-21 21:06:37 +0300

    Fix scroll button, don't know how i missed this.

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -12,17 +12,19 @@ const scrollToTop = () => {
     }
     
     useEffect(() => {
-        window.addEventListener('click', handlescrollToTop);
+        document.querySelector(".srollToTopButton").
+            addEventListener('click', handlescrollToTop);
     
         return () => {
-          window.removeEventListener('click', handlescrollToTop);
+            document.querySelector(".srollToTopButton").
+                removeEventListener('click', handlescrollToTop);
         };
       }, []);
 
     return (
         <>
             <div className={"w-[99.1vw] flex justify-center items-center mt-[3vh]"}>
-                <button type="button" className="w-[150px] px-1 py-2 bg-[#464646] rounded-lg justify-self-center hover:bg-[#585858]">Scroll back up?</button>
+                <button type="button" className="srollToTopButton w-[150px] px-1 py-2 bg-[#464646] rounded-lg justify-self-center hover:bg-[#585858]">Scroll back up?</button>
             </div>
         </>
     )
