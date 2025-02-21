381ecb6a54a8b9d96d2bfcdca18cb79e6a7c35d2
lidwan <contact@loayidwan.com>
2025-02-21 21:07:49 +0300

    Added correct links to postCards

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function PostCard({post}){
     return (
-        <Link href={"/"} className={"hover:no-underline text-white hover:text-white"}>
+        <Link href={"/posts/"+[post.id]} className={"hover:no-underline text-white hover:text-white"}>
             <div className="card flex flex-col justify-between gap-[1vh] px-[2vh] py-[3vh] text-center bg-[#212529] rounded-lg shadow-[5px_5px_10px_2px_#7f7f7f,inset_-5px_-4px_15px_3px_#3c3a3a] transition-all duration-300 hover:shadow-[10px_10px_15px_2px_#a4a4a4] hover:-translate-x-1.5 hover:-translate-y-1.5 " >
                 <div className="card-body">
                     <h5 className="font-bold pb-[1vh] text-xl">
