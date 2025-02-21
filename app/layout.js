22b6bdf0a365fe0050997002883406230b22a742
lidwan <contact@loayidwan.com>
2025-02-22 01:45:39 +0300

    Added vercel speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Loay's Blog",
@@ -38,6 +39,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
